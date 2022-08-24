4f46c3f3aae9197a96c4b922e873e47b9dd52900
ppseafield <ppseafield@gmail.com>
2022-08-24 10:07:24 -0700

    3 files modified

--- app/components/OrderDetailRow.jsx
+++ app/components/OrderDetailRow.jsx
@@ -1,34 +1,60 @@
-import React from 'react'
-import { Grid, GridColumn, GridDetailRow } from '@progress/kendo-react-grid'
+import React from 'react';
+import { Grid, GridColumn, GridDetailRow } from '@progress/kendo-react-grid';
 
 export default class OrderDetailRow extends GridDetailRow {
   render() {
     const details = this.props.dataItem.details.map(
       ({ productID, unitPrice, quantity, discount }) => {
-      const itemTotal =  unitPrice * quantity * (1 - discount)
-      return {
-        productID,
-        unitPrice,
-        quantity,
-        discountPercent: `${discount * 100}%`,
-        itemTotal
+        const itemTotal = unitPrice * quantity * (1 - discount);
+        return {
+          productID,
+          unitPrice,
+          quantity,
+          discountPercent: `${discount * 100}%`,
+          itemTotal,
+        };
       }
-    })
-    const grandTotal = details.reduce((sum, detailItem) => sum + detailItem.itemTotal, 0)
+    );
+    const grandTotal = details.reduce(
+      (sum, detailItem) => sum + detailItem.itemTotal,
+      0
+    );
     return (
       <div>
-      <Grid data={details}>
-        <GridColumn field="productID" title="Product ID" />
-        <GridColumn field="unitPrice" title="Price" format="{0:c}"/>
-        <GridColumn field="quantity" title="Qty" />
-        <GridColumn field="discountPercent" title="Discount" />
-        <GridColumn field="itemTotal" title="Item Total"  format="{0:c}"/>
-      </Grid>
-      <p style={{'text-align': 'center'}}>
-        <strong>Grand Total: </strong>
-        ${grandTotal.toFixed(2)}
-      </p>
+        <Grid
+          data={details}
+          editField={'inEdit'}
+          onItemChange={this.props.onItemChange || null}
+        >
+          <GridColumn
+            field="productID"
+            title="Product ID"
+            editor="text"
+            editable={this.props.editable}
+          />
+          <GridColumn
+            field="unitPrice"
+            title="Price"
+            format="{0:c}"
+            editor="text"
+          />
+          <GridColumn field="quantity" title="Qty" editor="numeric" />
+          <GridColumn
+            field="discountPercent"
+            title="Discount"
+            editor="numeric"
+          />
+          <GridColumn
+            field="itemTotal"
+            title="Item Total"
+            format="{0:c}"
+            editor="text"
+          />
+        </Grid>
+        <p style={{ textAlign: 'center' }}>
+          <strong>Grand Total: </strong>${grandTotal.toFixed(2)}
+        </p>
       </div>
-    )
+    );
   }
 }
--- app/components/OrderDialog.jsx
+++ app/components/OrderDialog.jsx
@@ -1,57 +1,154 @@
-import React from 'react'
-import { Form, Fireld, FormElement, Field } from '@progress/kendo-react-form'
-import { Error } from '@progress/kendo-react-labels'
-import { Input } from '@progress/kendo-react-inputs'
-import { Dialog } from '@progress/kendo-react-dialogs'
+import React from 'react';
+import { Form, Fireld, FormElement, Field } from '@progress/kendo-react-form';
+import { GridLayout, GridLayoutItem } from '@progress/kendo-react-layout';
+import { Error } from '@progress/kendo-react-labels';
+import { Input } from '@progress/kendo-react-inputs';
+import { DatePicker } from '@progress/kendo-react-dateinputs';
+import { Dialog } from '@progress/kendo-react-dialogs';
+import OrderDetailRow from './OrderDetailRow.jsx';
 
 export default class OrderDialog extends React.Component {
-  dialogTitle () {
-    const { order } = this.props
-    return order?.orderID ? `Editing Order #${order.orderID}` : 'New Order'
+  state = {
+    dataItem: {
+      details: [
+        {
+          productID: 0,
+          unitPrice: 0.0,
+          quantity: 0,
+          discount: 0,
+          inEdit: true,
+        },
+      ],
+    },
   }
-  render () {
+  dialogTitle() {
+    const { order } = this.props;
+    return order?.orderID ? `Editing Order #${order.orderID}` : 'New Order';
+  }
+  handleSubmit() {
+    // collect detail items
+    this.props.handleSubmit();
+  }
+  render() {
     return (
       <Dialog title={this.dialogTitle()}>
         <Form
-        onSubmit="this.props.handleSubmit"
-        render={(formRenderProps) => (
-          <FormElement
-            style={{ maxWidth: "850px" }} 
-          >
-            <fieldset className={"k-form-fieldset"}>
-              <div className="mb-3">
-                <Field
-                  name="customerID"
-                  component={Input}
-                  label="Customer ID"
-                />
-              </div>
-              <div className="mb-3">
-                <Field
-                  name="employeeID"
-                  component={Input}
-                  label="Employee ID"
-                />
-              </div>
-              <div className="mb-3">
-                <Field
-                  name="orderDate"
-                  component={Input}
-                  label="OrderDate"
+          onSubmit={this.handleSubmit}
+          render={(formRenderProps) => (
+            <FormElement style={{ maxWidth: '1050px' }}>
+              <GridLayout
+                rows={[{ height: 580 }]}
+                cols={[{ width: 180 }, { width: 180 }, { width: 700 }]}
+                gap={{ rows: 0, cols: 20 }}
+              >
+                <GridLayoutItem>
+                  <fieldset className={'k-form-fieldset'}>
+                    <legend>Order Details</legend>
+                    <div className="mb-3">
+                      <Field
+                        name="customerID"
+                        component={Input}
+                        label="Customer ID"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="employeeID"
+                        component={Input}
+                        label="Employee ID"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="orderDate"
+                        component={DatePicker}
+                        label="Order Date"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="requiredDate"
+                        component={DatePicker}
+                        label="Required Date"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipDate"
+                        component={DatePicker}
+                        label="Ship Date"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipVia"
+                        component={Input}
+                        label="Ship Via"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field name="freight" component={Input} label="Freight" />
+                    </div>
+                  </fieldset>
+                </GridLayoutItem>
+                <GridLayoutItem>
+                  <fieldset className="k-form-fieldset">
+                    <legend>Address</legend>
+                    <div className="mb-3">
+                      <Field
+                        name="shipName"
+                        component={Input}
+                        label="Ship To"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipAddress.street"
+                        component={Input}
+                        label="Street Address"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipAddress.city"
+                        component={Input}
+                        label="City"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipAddress.region"
+                        component={Input}
+                        label="State/Region"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipAddress.postalCode"
+                        component={Input}
+                        label="Postal Code/ZIP"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <Field
+                        name="shipAddress.country"
+                        component={Input}
+                        label="Country"
+                      />
+                    </div>
+                  </fieldset>
+                </GridLayoutItem>
+                <GridLayoutItem>
+                  <OrderDetailRow
+                    dataItem={this.state.dataItem}
+                    editable={true}
                   />
-              </div>
-              <div className="mb-3">
-                <Field
-                  name="shipName"
-                  component={Input}
-                  label="Ship To"
-                />
-              </div>
-            </fieldset>
-          </FormElement>
-        )}
-      />
+                </GridLayoutItem>
+              </GridLayout>
+            </FormElement>
+          )}
+        />
       </Dialog>
-    )
+    );
   }
 }
